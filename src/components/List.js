e5af7be2312767555efa45a652e09e8476637445
eraygundogmus <gundogmuseray@gmail.com>
2021-04-03 23:15:20 +0300

    style fix

--- src/components/List.js
+++ src/components/List.js
@@ -60,29 +60,40 @@ function List() {
               </div>
             </div> */}
             {isTodoFormOpen ? (
-              <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-40 shadow-xl">
-                Modal
-                <form onSubmit={onSubmit}>
-                  <input
-                    value={todoText}
-                    required="required"
-                    onChange={(event) => setTodoText(event.target.value)}
-                    placeholder="Enter a title for card"
-                  ></input>
-                  <input
-                    type="date"
-                    id="start"
-                    name="trip-start"
-                    value={todoDate}
-                    min="2021-01-01"
-                    max="2022-12-31"
-                    onChange={(event) => setTodoDate(event.target.value)}
-                  ></input>
-                  <button className="main" type="submit">
-                    Submit
-                  </button>
-                  <button onClick={() => setisTodoFormOpen(false)}> X </button>
-                </form>
+              <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-30 shadow-xl">
+                <div className="bg-white rounded-xl items-center m-64 flex">
+                  <form
+                    className="justify-center flex w-full h-full"
+                    onSubmit={onSubmit}
+                  >
+                    <input
+                      value={todoText}
+                      required="required"
+                      onChange={(event) => setTodoText(event.target.value)}
+                      placeholder="Enter a title for card"
+                      className="px-2  w-full pr-2 rounded-xl  text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
+                    ></input>
+                    <input
+                      type="date"
+                      id="start"
+                      name="trip-start"
+                      value={todoDate}
+                      min="2021-01-01"
+                      max="2022-12-31"
+                      className="w-full rounded-xl  text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
+                      onChange={(event) => setTodoDate(event.target.value)}
+                    ></input>
+                    <button className="main" type="submit">
+                      Submit
+                    </button>
+                    <button
+                      className="main"
+                      onClick={() => setisTodoFormOpen(false)}
+                    >
+                      Close
+                    </button>
+                  </form>
+                </div>
               </div>
             ) : null}
           </div>
@@ -97,18 +108,24 @@ function List() {
         </button>
       </div>
       {isNewListOpen ? (
-        <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-40 shadow-xl">
-          <form onSubmit={newCardSubmit}>
-            <input
-              value={newListName}
-              onChange={(event) => setNewListName(event.target.value)}
-              placeholder="Enter list name"
-            ></input>
-            <button className="main"> Add</button>
-          </form>
-          <button className="main" onClick={() => setisNewListOpen(false)}>
-            Close
-          </button>
+        <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-30 shadow-xl">
+          <div className="bg-white rounded-xl items-center m-64 flex">
+            <form
+              className="w-full justify-between flex h-full"
+              onSubmit={newCardSubmit}
+            >
+              <input
+                value={newListName}
+                onChange={(event) => setNewListName(event.target.value)}
+                placeholder="Enter list name"
+                className="px-2 pr-2 rounded-xl w-full text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
+              ></input>
+              <button className="main"> Add</button>
+            </form>
+            <button className="main" onClick={() => setisNewListOpen(false)}>
+              Close
+            </button>
+          </div>
         </div>
       ) : null}
     </div>
