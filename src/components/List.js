211d9a7e0339720f0dcd06fc8d5887dfa2820890
eraygundogmus <gundogmuseray@gmail.com>
2021-04-04 15:42:59 +0300

    add dnd

--- src/components/List.js
+++ src/components/List.js
@@ -3,6 +3,7 @@ import { trelloContext } from "../context/trelloContext";
 import ListItem from "./ListItem";
 import { v4 as uuidv4 } from "uuid";
 import { MdMoreHoriz } from "react-icons/md";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function List() {
   const myContext = useContext(trelloContext);
@@ -44,107 +45,118 @@ function List() {
     setisNewListOpen(false);
   };
 
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    console.log("destionation", destination, "source", source, draggableId);
+  };
+
   return (
-    <div className="myGrid">
-      {obj.map((trello, index) => (
-        <div className="myList">
-          <div className="flex justify-between items-center	">
-            <h2 className="font-semibold py-2">{trello}</h2>
-            <MdMoreHoriz />
-          </div>
-          <div>
-            {myContext.trellos[trello].map((item) => (
-              <ListItem trello={trello} drill={item} />
-            ))}
-            {/*             <div className="single">
-              <div className="single-open">
-                <p className="text">Add new</p>
-              </div>
-            </div> */}
-            {isTodoFormOpen ? (
-              <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-30 shadow-xl">
-                <div className="bg-white rounded-xl items-center mx-24 my-64 flex">
-                  <form
-                    className="justify-center flex w-full h-full"
-                    onSubmit={onSubmit}
-                  >
-                    <input
-                      value={todoText}
-                      required="required"
-                      onChange={(event) => setTodoText(event.target.value)}
-                      placeholder="Enter a title for card"
-                      className="px-6  w-full  min-w-min pr-2  rounded-xl  text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
-                    ></input>
-                    <input
-                      type="date"
-                      id="start"
-                      name="trip-start"
-                      value={todoDate}
-                      min="2021-01-01"
-                      max="2022-12-31"
-                      className="px-6 w-full rounded-xl min-w-min text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
-                      onChange={(event) => setTodoDate(event.target.value)}
-                    ></input>
-                    <select
-                      onChange={(event) => setParentName(event.target.value)}
-                      value={parentName}
-                      name="parent"
-                      id="parent"
-                      required="required"
-                    >
-                      <option disabled selected>
-                        Select a list
-                      </option>
-                      {obj.map((trello) => (
-                        <option>{trello}</option>
-                      ))}
-                    </select>
-                    <button className="main" type="submit">
-                      Submit
-                    </button>
-                    <button
-                      className="main"
-                      onClick={() => setisTodoFormOpen(false)}
-                    >
-                      Close
-                    </button>
-                  </form>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="myGrid">
+        {obj.map((trello, index) => (
+          <Droppable droppableId={index}>
+            {(provided) => (
+              <div
+                className="myList"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                <div className="flex justify-between items-center	">
+                  <h2 className="font-semibold py-2">{trello}</h2>
+                  <MdMoreHoriz />
                 </div>
+                {myContext.trellos[trello].map((item, i) => (
+                  <ListItem trello={trello} drill={item} />
+                ))}
+                {isTodoFormOpen ? (
+                  <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-30 shadow-xl">
+                    <div className="bg-white rounded-xl items-center mx-24 my-64 flex">
+                      <form
+                        className="justify-center flex w-full h-full"
+                        onSubmit={onSubmit}
+                      >
+                        <input
+                          value={todoText}
+                          required="required"
+                          onChange={(event) => setTodoText(event.target.value)}
+                          placeholder="Enter a title for card"
+                          className="px-6  w-full  min-w-min pr-2  rounded-xl  text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
+                        ></input>
+                        <input
+                          type="date"
+                          id="start"
+                          name="trip-start"
+                          value={todoDate}
+                          min="2021-01-01"
+                          max="2022-12-31"
+                          className="px-6 w-full rounded-xl min-w-min text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
+                          onChange={(event) => setTodoDate(event.target.value)}
+                        ></input>
+                        <select
+                          onChange={(event) =>
+                            setParentName(event.target.value)
+                          }
+                          value={parentName}
+                          name="parent"
+                          id="parent"
+                          required="required"
+                        >
+                          <option disabled selected>
+                            Select a list
+                          </option>
+                          {obj.map((trello) => (
+                            <option>{trello}</option>
+                          ))}
+                        </select>
+                        <button className="main" type="submit">
+                          Submit
+                        </button>
+                        <button
+                          className="main"
+                          onClick={() => setisTodoFormOpen(false)}
+                        >
+                          Close
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                ) : null}
               </div>
-            ) : null}
-          </div>
+            )}
+          </Droppable>
+        ))}
+
+        <div className="flex justify-center">
+          <button onClick={() => setisTodoFormOpen(true)} className="main m-2">
+            Create card
+          </button>
+          <button className="main m-2" onClick={() => setisNewListOpen(true)}>
+            Create list
+          </button>
         </div>
-      ))}
-      <div className="flex justify-center">
-        <button onClick={() => setisTodoFormOpen(true)} className="main m-2">
-          Create card
-        </button>
-        <button className="main m-2" onClick={() => setisNewListOpen(true)}>
-          Create list
-        </button>
+        {isNewListOpen ? (
+          <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-30 shadow-xl">
+            <div className="bg-white rounded-xl items-center  mx-24 my-64 flex">
+              <form
+                className="w-full justify-between flex h-full"
+                onSubmit={newCardSubmit}
+              >
+                <input
+                  value={newListName}
+                  onChange={(event) => setNewListName(event.target.value)}
+                  placeholder="Enter list name"
+                  className="px-6 pr-2 rounded-xl w-full text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
+                ></input>
+                <button className="main"> Add</button>
+              </form>
+              <button className="main" onClick={() => setisNewListOpen(false)}>
+                Close
+              </button>
+            </div>
+          </div>
+        ) : null}
       </div>
-      {isNewListOpen ? (
-        <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-30 shadow-xl">
-          <div className="bg-white rounded-xl items-center  mx-24 my-64 flex">
-            <form
-              className="w-full justify-between flex h-full"
-              onSubmit={newCardSubmit}
-            >
-              <input
-                value={newListName}
-                onChange={(event) => setNewListName(event.target.value)}
-                placeholder="Enter list name"
-                className="px-6 pr-2 rounded-xl w-full text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
-              ></input>
-              <button className="main"> Add</button>
-            </form>
-            <button className="main" onClick={() => setisNewListOpen(false)}>
-              Close
-            </button>
-          </div>
-        </div>
-      ) : null}
-    </div>
+    </DragDropContext>
   );
 }
 
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { trelloContext } from "../context/trelloContext";
 import { MdDateRange } from "react-icons/md";
+import { Draggable } from "react-beautiful-dnd";
 
 function ListItem(props) {
   const [isTodoOpen, setisTodoOpen] = useState(false);
@@ -43,108 +44,117 @@ function ListItem(props) {
   };
 
   return (
-    <div className="single">
-      {isEditOpen ? (
-        <div className="single-open">
-          <div className="relative max-w-full">
-            <p className="text" onClick={() => setisTodoOpen(true)}>
-              {data.text}
-            </p>
-            {data.deadline ? (
-              <div className="mx-2 mb-2 flex">
-                <MdDateRange className="fill-current text-green-700" />
-                <h4 className="px-1 text-left text-xs text-gray-600 break-words ">
-                  {data.deadline}
-                </h4>
+    <Draggable draggableId={`${data.id}ahaha`} index={data.id}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          className="single"
+        >
+          {isEditOpen ? (
+            <div className="single-open">
+              <div className="relative max-w-full">
+                <p className="text" onClick={() => setisTodoOpen(true)}>
+                  {data.text}
+                </p>
+                {data.deadline ? (
+                  <div className="mx-2 mb-2 flex">
+                    <MdDateRange className="fill-current text-green-700" />
+                    <h4 className="px-1 text-left text-xs text-gray-600 break-words ">
+                      {data.deadline}
+                    </h4>
+                  </div>
+                ) : null}
               </div>
-            ) : null}
-          </div>
-          <div className="absolute top-3 right-2">
-            <AiFillEdit
-              className="fill-current cursor-pointer text-green-700"
-              onClick={(event) => setisEditOpen(false)}
-            />
-          </div>
-        </div>
-      ) : (
-        <div className="bg-white flex rounded-xl  border border-gray-300 shadow my-1 relative ">
-          <form
-            className="w-full justify-between flex"
-            onSubmit={handleEditForm}
-          >
-            <input
-              type="text"
-              required="required"
-              placeholder={data.text}
-              onChange={(event) => setEditText(event.target.value)}
-              value={editText}
-              className="px-2 pr-2  rounded-xl w-full text-xs h-full focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
-            ></input>
-            <button type="submit" className="main">
-              Save
-            </button>
-          </form>
-
-          <button className="px-3" onClick={(event) => setisEditOpen(true)}>
-            X
-          </button>
-        </div>
-      )}
-
-      {isTodoOpen ? (
-        <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-30 shadow-xl">
-          <div className="bg-white items-center m-64">
-            <div className="relative">
-              <p className="text-center text-gray-700  font-semibold py-2">
-                {data.text}
-              </p>
-              <p className="text-center text-gray-700  text-sm font-light">
-                In the list: {props.trello}
-              </p>
+              <div className="absolute top-3 right-2">
+                <AiFillEdit
+                  className="fill-current cursor-pointer text-green-700"
+                  onClick={(event) => setisEditOpen(false)}
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="bg-white flex rounded-xl  border border-gray-300 shadow my-1 relative ">
               <form
-                className="justify-center flex w-full h-full"
-                onSubmit={upDateForm}
+                className="w-full justify-between flex"
+                onSubmit={handleEditForm}
               >
                 <input
-                  type="date"
-                  id="start"
-                  name="trip-start"
+                  type="text"
                   required="required"
-                  value={upDate}
-                  min="2021-01-01"
-                  max="2022-12-31"
-                  className="px-6 w-full rounded-xl min-w-min text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
-                  onChange={(event) => setupDate(event.target.value)}
+                  placeholder={data.text}
+                  onChange={(event) => setEditText(event.target.value)}
+                  value={editText}
+                  className="px-2 pr-2  rounded-xl w-full text-xs h-full focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
                 ></input>
                 <button type="submit" className="main">
-                  Update
+                  Save
                 </button>
               </form>
-              <AiFillDelete onClick={handleDeleteItem} />
-              {data.members ? (
-                <div className="px-4 font-bold text-sm text-gray-700">
-                  Members
-                  {data.members
-                    ? data.members.map((member) => (
-                        <p className="text-gray-700 text-sm  font-normal">
-                          {member}
-                        </p>
-                      ))
-                    : null}
-                </div>
-              ) : null}
 
-              <button
-                className="absolute top-2 right-3 font-semibold	"
-                onClick={() => setisTodoOpen(false)}
-              >
+              <button className="px-3" onClick={(event) => setisEditOpen(true)}>
                 X
               </button>
             </div>
-          </div>
+          )}
+
+          {isTodoOpen ? (
+            <div className="z-10 bg-gray-500 border border-gray-500 absolute w-full h-full inset-0 bg-opacity-30 shadow-xl">
+              <div className="bg-white items-center m-64">
+                <div className="relative">
+                  <p className="text-center text-gray-700  font-semibold py-2">
+                    {data.text}
+                  </p>
+                  <p className="text-center text-gray-700  text-sm font-light">
+                    In the list: {props.trello}
+                  </p>
+                  <form
+                    className="justify-center flex w-full h-full"
+                    onSubmit={upDateForm}
+                  >
+                    <input
+                      type="date"
+                      id="start"
+                      name="trip-start"
+                      required="required"
+                      value={upDate}
+                      min="2021-01-01"
+                      max="2022-12-31"
+                      className="px-6 w-full rounded-xl min-w-min text-xs focus:outline-none focus:ring-1 ring-green-700 focus:border-green-500"
+                      onChange={(event) => setupDate(event.target.value)}
+                    ></input>
+                    <button type="submit" className="main">
+                      Update
+                    </button>
+                  </form>
+                  <AiFillDelete onClick={handleDeleteItem} />
+                  {data.members ? (
+                    <div className="px-4 font-bold text-sm text-gray-700">
+                      Members
+                      {data.members
+                        ? data.members.map((member) => (
+                            <p className="text-gray-700 text-sm  font-normal">
+                              {member}
+                            </p>
+                          ))
+                        : null}
+                    </div>
+                  ) : null}
+
+                  <button
+                    className="absolute top-2 right-3 font-semibold	"
+                    onClick={() => setisTodoOpen(false)}
+                  >
+                    X
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : null}
         </div>
-      ) : null}
-    </div>
+      )}
+    </Draggable>
   );
 }
 
